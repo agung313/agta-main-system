ba2a97aec739c5de5c7d59d26342363d2dc8c8c2
MuhammadAgungSholihhudin <63606892+agung313@users.noreply.github.com>
2025-01-17 20:57:06 +0700

    [update] - api contact admin

--- src/app/admin/layouts/Contact.tsx
+++ src/app/admin/layouts/Contact.tsx
@@ -1,91 +1,203 @@
-import React, { useState } from 'react'
-import DivContent from '../components/DivContent';
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Image from 'next/image';
+import { useDispatch, useSelector } from 'react-redux';
+import DivContent from '../components/DivContent';
 import Email from '../../icons/email.svg';
 import Instagram from '../../icons/instagram.svg';
 import Linkedin from '../../icons/linkedin.svg';
 import Maps from '../../icons/maps.svg';
+import Telephone from '../../icons/telephone.svg';
 import InputContent from '../components/InputContent';
+import LoadingPage from '@/app/components/LoadingPage';
+import { getContact, updateContact } from '@/app/api/admin';
+import { hideLoadingSubmit, showLoadingSubmit } from '@/app/redux/admin';
+import { showNotification } from '@/app/redux/components';
 
 const Contact = () => {
-  const [contactData, setContactData] = useState([
-    {
-      img: Email,
-      title: 'Email',
-      text: 'sholihhudinagung@gmail.com',
-    },
-    {
-      img: Instagram,
-      title: 'Instagram',
-      text: 'sholihhudinagung',
-    },
-    {
-      img: Linkedin,
-      title: 'Linkedin',
-      text: 'muhammad agung sholihhudin',
-    },
-    {
-      img: Maps,
-      title: 'Address',
-      text: 'Jl. Daru - Daru VII, Pekanbaru, Riau, Indonesia',
-    },
-  ]);
-  const [googleMapsLink, setGoogleMapsLink] = useState('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.6816889525358!2d101.4941733!3d0.47417380000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d5afb14e200015%3A0x32f1a877bc30b00c!2sJl.%20Daru-Daru%20VII%2C%20Tengkerang%20Tim.%2C%20Kec.%20Tenayan%20Raya%2C%20Kota%20Pekanbaru%2C%20Riau%2028289!5e0!3m2!1sid!2sid!4v1734322498967!5m2!1sid!2sid');
-  const [linkFrame, setLinkFrame] = useState(googleMapsLink);
+  const dispatch = useDispatch();
+  const isLoadingSubmit = useSelector((state: { admin: { isLoadingSubmit: boolean } }) => state.admin.isLoadingSubmit);
+  const isMounted = useRef(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [contactData, setContactData] = useState({
+    email: '',
+    address: '',
+    addressLink: '',
+    instagram: '',
+    linkedinLink: '',
+    phone: '',
+  });
+
+  const getServiceData = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const res = await getContact();
+      setContactData({...res.data.data});
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    } catch (error) {
+      console.log('cek error', error); // eslint-disable-line
+      setIsLoading(false);
+    }
+  }, [setIsLoading]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      getServiceData();
+    }
+    return () => {
+      isMounted.current = false
+    }
+  }, [getServiceData]);
+
+  const handleUpdateContact = async () => {
+    dispatch(showLoadingSubmit());
+    try {
+      await updateContact(contactData);
+      setTimeout(() => {
+        dispatch(showNotification({ message: { id: 'Selamat, Contact berhasil diperbarui', en: 'Congratulations, Contact successfully updated' }, type: 'success' }));
+        dispatch(hideLoadingSubmit());
+      }, 1000);
+    } catch (error) {
+      console.log('cek error', error); // eslint-disable-line
+      dispatch(showNotification({ message: { id: 'Maaf, Contact gagal diperbarui', en: 'Sorry, Contact failed to update' }, type: 'failed' }));
+      dispatch(hideLoadingSubmit());
+    }
+  };
 
   return (
-    <div className='w-full p-5 flex flex-col'>
-      <DivContent>
-        <p className='font-extrabold text-neutral-300 text-[3vh] mb-10'>Contacts</p>
-        <div className='grid grid-cols-2 gap-5'>
-          {contactData.map((contact, index) => (
-            <div key={index} className='flex items-center'>
-              <div className='w-[10%]'>
-                <Image src={contact.img} alt="Logo" className="w-[2vh] md:w-[3vh] lg:w-[4vh] h-auto" />
+    <div>
+      {isLoading
+        ?
+          <div className='flex w-full h-[90vh] justify-center items-center'>
+            <LoadingPage color='#fff' size={70} isLoading={isLoading} />
+          </div>
+        :
+        <div className='w-full p-5 flex flex-col'>
+          <DivContent>
+            <p className='font-extrabold text-neutral-300 text-[3vh] mb-10'>Contacts</p>
+            <div className='grid grid-cols-2 gap-5'>
+
+              <div className='flex items-center'>
+                <div className='w-[10%]'>
+                  <Image src={Email} alt="Logo" className="w-[5vh] h-auto" />
+                </div>
+                <div className="ml-4 w-[90%]">
+                  <p className="text-purple-400 text-[2.5vh] mb-2">Email</p>
+                  <InputContent
+                    disabled={isLoadingSubmit}
+                    id='contact-email'
+                    type='text'
+                    placeholder='Type email here...'
+                    value={contactData.email}
+                    setValue={value => setContactData({ ...contactData, email: value })}
+                    classNameInput='text-[2.5vh] text-white border-none mb-0 w-full p-0'
+                  />
+                </div>
               </div>
-              <div className="ml-4 w-[90%]">
-                <p className="text-purple-400 text-[2.5vh] mb-2">{contact.title}</p>
-                <InputContent
-                  id={`contact-${index}`}
-                  type='text'
-                  value={contact.text}
-                  setValue={value => {
-                    const newContactData = [...contactData];
-                    newContactData[index].text = value;
-                    setContactData(newContactData);
-                  }}
-                  classNameInput='text-[2.5vh] text-white border-none mb-0 w-full p-0'
-                />
+              
+              <div className='flex items-center'>
+                <div className='w-[10%]'>
+                  <Image src={Instagram} alt="Logo" className="w-[5vh] h-auto" />
+                </div>
+                <div className="ml-4 w-[90%]">
+                  <p className="text-purple-400 text-[2.5vh] mb-2">Instagram</p>
+                  <InputContent
+                    disabled={isLoadingSubmit}
+                    id='contact-instagram'
+                    type='text'
+                    placeholder='Type instagram here...'
+                    value={contactData.instagram}
+                    setValue={value => setContactData({ ...contactData, instagram: value })}
+                    classNameInput='text-[2.5vh] text-white border-none mb-0 w-full p-0'
+                  />
+                </div>
               </div>
+
+              <div className='flex items-center'>
+                <div className='w-[10%]'>
+                  <Image src={Linkedin} alt="Logo" className="w-[5vh] h-auto" />
+                </div>
+                <div className="ml-4 w-[90%]">
+                  <p className="text-purple-400 text-[2.5vh] mb-2">Linkedin Link</p>
+                  <InputContent
+                    disabled={isLoadingSubmit}
+                    id='contact-Linkedin'
+                    type='text'
+                    placeholder='Type linkedin link here...'
+                    value={contactData.linkedinLink}
+                    setValue={value => setContactData({ ...contactData, linkedinLink: value })}
+                    classNameInput='text-[2.5vh] text-white border-none mb-0 w-full p-0'
+                  />
+                </div>
+              </div>
+              
+              <div className='flex items-center'>
+                <div className='w-[10%]'>
+                  <Image src={Maps} alt="Logo" className="w-[5vh] h-auto" />
+                </div>
+                <div className="ml-4 w-[90%]">
+                  <p className="text-purple-400 text-[2.5vh] mb-2">Address</p>
+                  <InputContent
+                    disabled={isLoadingSubmit}
+                    id='contact-address'
+                    type='text'
+                    placeholder='Type address here...'
+                    value={contactData.address}
+                    setValue={value => setContactData({ ...contactData, address: value })}
+                    classNameInput='text-[2.5vh] text-white border-none mb-0 w-full p-0'
+                  />
+                </div>
+              </div>
+              
+              <div className='flex items-center'>
+                <div className='w-[10%]'>
+                  <Image src={Maps} alt="Logo" className="w-[5vh] h-auto" />
+                </div>
+                <div className="ml-4 w-[90%]">
+                  <p className="text-purple-400 text-[2.5vh] mb-2">Address Link</p>
+                  <InputContent
+                    disabled={isLoadingSubmit}
+                    id='contact-addressLink'
+                    type='text'
+                    placeholder='Type address link here...'
+                    value={contactData.addressLink}
+                    setValue={value => setContactData({ ...contactData, addressLink: value })}
+                    classNameInput='text-[2.5vh] text-white border-none mb-0 w-full p-0'
+                  />
+                </div>
+              </div>
+              
+              <div className='flex items-center'>
+                <div className='w-[10%]'>
+                  <Image src={Telephone} alt="Logo" className="w-[5vh] h-auto" />
+                </div>
+                <div className="ml-4 w-[90%]">
+                  <p className="text-purple-400 text-[2.5vh] mb-2">Telephone</p>
+                  <InputContent
+                    disabled={isLoadingSubmit}
+                    id='contact-phone'
+                    type='text'
+                    placeholder='Type telephone here...'
+                    value={contactData.phone}
+                    setValue={value => setContactData({ ...contactData, phone: value })}
+                    classNameInput='text-[2.5vh] text-white border-none mb-0 w-full p-0'
+                  />
+                </div>
+              </div>
+
             </div>
-          ))}
+          </DivContent>
+          <button
+            type="submit"
+            className="my-10 text-[2vh] w-full font-extrabold p-2 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white rounded-md hover:bg-gradient-to-r hover:from-purple-600 hover:via-pink-700 hover:to-red-700"
+            onClick={handleUpdateContact}
+          >
+            Save Changes
+          </button>
         </div>
-        <div className='mt-16'>
-          <div className='mb-5 flex items-center justify-between'>
-            <p className="text-purple-400 text-[2.5vh]">Link Google Maps</p>
-            <button onClick={() => setLinkFrame(googleMapsLink)} className='bg-clip-border bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 font-bold text-white px-2 py-1 rounded-md hover:bg-gradient-to-r hover:from-purple-600 hover:via-pink-700 hover:to-red-700'>
-              Check Link
-            </button>
-          </div>
-          <InputContent
-            id='googleMapsLink'
-            type='text'
-            value={googleMapsLink}
-            setValue={value => setGoogleMapsLink(value)}
-            classNameInput='text-[2.5vh] text-white border-none mb-5 w-full p-0'
-            rows={6}
-          />
-          <div className="order-1 sm:order-2">
-            <iframe src={linkFrame} width="100%" height="400" loading="lazy" referrerPolicy="no-referrer-when-downgrade" style={{ backgroundColor: "#2c2c2c" }}/>
-          </div>
-        </div>
-      </DivContent>
-      <button
-        type="submit"
-        className="my-10 text-[2vh] w-full font-extrabold p-2 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white rounded-md hover:bg-gradient-to-r hover:from-purple-600 hover:via-pink-700 hover:to-red-700"
-      >
-        Save Changes
-      </button>
+      }
     </div>
   );
 };
